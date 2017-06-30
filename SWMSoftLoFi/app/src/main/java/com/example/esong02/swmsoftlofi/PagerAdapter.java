d07768d325b5a93217367e4b5de189c35d3d1758
Eric Song <esong02@Feis-MBP-2.Civica.local>
2017-06-30 10:21:17 -0400

    Filtering technically works but is not updated immediately. Will work on fixing this.

--- SWMSoftLoFi/app/src/main/java/com/example/esong02/swmsoftlofi/PagerAdapter.java
+++ SWMSoftLoFi/app/src/main/java/com/example/esong02/swmsoftlofi/PagerAdapter.java
@@ -3,13 +3,25 @@ package com.example.esong02.swmsoftlofi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.Log;
+
+import java.util.List;
 
 public class PagerAdapter extends FragmentStatePagerAdapter {
     int mNumOfTabs;
+    private List<String> passList;
+    private boolean filterNow = false;
+    //private FacilityTab tab1;
 
     public PagerAdapter(FragmentManager fm, int NumOfTabs) {
         super(fm);
         this.mNumOfTabs = NumOfTabs;
+    }
+
+    public void setFilter(List<String> pList){
+        filterNow = true;
+        passList = pList;
+        //Log.d("Filter", "" + passList.size());
     }
 
     @Override
@@ -18,6 +30,16 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
         switch (position) {
             case 0:
                 FacilityTab tab1 = new FacilityTab();
+                //tab1.filterFS = passList;
+                /*
+                if (filterNow = true) {
+                    tab1.passList(passList, filterNow);
+                }else{
+                    tab1.passList(null, false);
+                    tab1.clearFilter();
+                }
+                */
+
                 return tab1;
             case 1:
                 StructureTab tab2 = new StructureTab();
